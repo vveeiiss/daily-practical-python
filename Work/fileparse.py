50a20685e74a58059bdbd93b301fc5a5b65a80a8
Melika <melika.411@gmail.com>
2024-02-13 19:05:45 +0100

    first exercises

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,35 @@
-# fileparse.py
-#
-# Exercise 3.3
+import csv
+
+def parse_csv(line, select=None):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    rows = []
+    for l in line:
+        rows.append(l)
+
+        # Read the file headers
+    headers = rows[0]
+    records = []
+    if select:
+        ind = [headers.index(column) for column in select]
+        headers = select
+    else:
+        ind = []
+
+    for row in rows:
+        print(row)
+        if not row:    # Skip rows with no data
+            continue
+
+        if ind:
+            row = [rows[index] for index in ind]
+        record = dict(zip(headers, row))
+        print(record)
+        records.append(record)
+
+    return rows
+
+with open('Data/portfolio.csv') as f:
+    d = parse_csv(f, ['name'])
+# print(d)
--- Work/pcost.py
+++ Work/pcost.py
@@ -5,8 +5,11 @@ def portfolio_cost(filename):
     headers = next(f).split(',')
     row = csv.reader(f)
     price = []
-    for i in row:
-        price.append(float(i[2]))
+    for num, i in enumerate(row, start=1):
+        try:
+            price.append(float(i[2]))
+        except ValueError:
+            print(f'Row {num}: Bad row: {i}')
     return sum(price)
 
-print(portfolio_cost('Data/portfolio.csv'))
+print(portfolio_cost('Data/missing.csv'))
